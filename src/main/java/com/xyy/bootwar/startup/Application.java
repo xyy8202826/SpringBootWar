50431d6cab16b5db76ebe303b4e8471e0b7b326c
Ryze <Ryze@xigualicai.com>
2015-12-27 12:50:21 +0800

    update

--- src/main/java/com/xyy/bootwar/startup/Application.java
+++ src/main/java/com/xyy/bootwar/startup/Application.java
@@ -1,21 +1,23 @@
 package com.xyy.bootwar.startup;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableJpaRepositories(basePackages={"com.xyy.bootwar.dao"})
-@EntityScan(basePackages="com.xyy.bootwar")
+@EntityScan(basePackages="com.xyy.bootwar.model")
+@ComponentScan("com.xyy.bootwar")
 public class Application extends SpringBootServletInitializer{
 	 @Override
 	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	    return application.sources(Application.class);
 	  }
-	 public static void main(String[] args) {
+	 public static void main(String[] args) throws Exception {
 	        SpringApplication.run(Application.class, args);
 	    }
 }
